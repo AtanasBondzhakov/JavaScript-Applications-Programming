800bbbc4d8df463a7f2aa73642b1ee4f3b5d571b
Atanas Bondzhakov <atanas.b@yahoo.com>
2024-11-05 21:00:58 +0200

    Create Utils

---
+++ 5. Architecture and Testing - Exercises/src/utils.js
@@ -0,0 +1,23 @@
+const sections = [...document.querySelectorAll('div[data-section]')];
+
+function hideAll() {
+    sections.forEach(section => section.style.display = 'none');
+}
+
+export function showView(section) {
+    document.querySelector('main').replaceChildren(section);
+}
+
+export function updateNav() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const userNav = [...document.querySelectorAll('[data-nav="user"]')];
+    const guestNav = [...document.querySelectorAll('[data-nav="guest"]')];
+
+    if(user) {
+        guestNav.forEach(el => el.style.display = 'none');
+        userNav.forEach(el => el.style.display = 'block');
+    } else {
+        userNav.forEach(el => el.style.display = 'none');
+        guestNav.forEach(el => el.style.display = 'block');
+    }
+}
