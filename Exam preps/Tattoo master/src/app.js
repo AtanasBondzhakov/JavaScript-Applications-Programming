62001749aac7e989402a63846a8fea8e3b959ab0
Atanas Bondzhakov <atanas.b@yahoo.com>
2024-11-08 00:00:38 +0200

    Add details view and logic

--- Exam preps/Tattoo master/src/app.js
+++ Exam preps/Tattoo master/src/app.js
@@ -10,6 +10,7 @@ import { renderRegister } from './views/registerView.js';
 import { onLogout } from './services/userServices.js';
 import { renderDashboard } from './views/dashboardView.js';
 import { renderCreate } from './views/createView.js';
+import { renderDetails } from './views/detailsView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
@@ -21,5 +22,6 @@ page('/register', renderRegister);
 page('/logout', onLogout);
 page('/dashboard', renderDashboard);
 page('/create', renderCreate);
+page('/details/:id', renderDetails);
 
 page.start();
---
+++ Exam preps/Tattoo master/src/views/detailsView.js
@@ -0,0 +1,50 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+import { getTattooById } from "../services/dataServices.js";
+import { getUserData, hasOwner } from "../utils.js";
+
+const detailsTemplate = (tattoo, hasOwner, hasUser) => html`
+        <section id="details">
+          <div id="details-wrapper">
+            <img
+              id="details-img"
+              src="./images/japanese dragon.png"
+              alt="example1"
+            />
+            <div>
+              <div id="info-wrapper">
+                <p id="details-type">${tattoo.type}</p>
+                <div id="details-description">
+                  <p id="user-type">${tattoo.userType}</p>
+                  <p id="description">
+                    ${tattoo.description}
+                  </p>
+                </div>
+                <h3>Like tattoo:<span id="like">0</span></h3>
+                ${hasUser ? html`
+                <div id="action-buttons">
+                    ${hasOwner ? html`
+                                <a href="#" id="edit-btn">Edit</a>
+                                <a href="#" id="delete-btn">Delete</a>`
+                               : html `<a href="#" id="like-btn">Like</a>`
+                    }
+                
+                </div>`: null}
+              </div>
+            </div>
+          </div>
+        </section>
+`;
+
+export const renderDetails = async (ctx) => {
+    const tattooId = ctx.params.id;
+    const userId = ctx.user?._id;
+
+    const tattoo = await getTattooById(tattooId);
+    const ownerId = tattoo._ownerId;
+
+    const hasUser = !!getUserData();
+    debugger
+
+    ctx.render(detailsTemplate(tattoo, hasOwner(userId, ownerId), hasUser));
+}
