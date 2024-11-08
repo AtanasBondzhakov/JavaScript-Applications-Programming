9ceb1c6349f02a09caf98796d2c44ccaf6b63c26
atanasbondzhakov <atanas.b@yahoo.com>
2024-11-08 10:31:06 +0200

    Add Navigation view functionality

---
+++ Exam preps/Renew Tech Cleanup/src/views/navigationView.js
@@ -0,0 +1,29 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const navigationTemplate = (user) => html`
+        <a id="logo" href="/"
+          ><img id="logo-img" src="./images/logo2.png" alt="logo"/>
+        </a>
+        <nav>
+          <div>
+            <a href="#">Solutions</a>
+          </div>
+
+          ${user ? userNavTemplate() : guestNavTemplate()}
+
+        </nav>
+`;
+
+const userNavTemplate = () => html`
+<div class="user">
+    <a href="/create">Add Solution</a>
+    <a href="/logout">Logout</a>
+</div>
+`;
+
+const guestNavTemplate = () => html`
+<div class="guest">
+  <a href="/login">Login</a>
+  <a href="/register">Register</a>
+</div>
+`;
