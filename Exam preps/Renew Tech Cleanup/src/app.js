b0ae04084e1dd1b0cbc65a9be16527ecde7bc83d
atanasbondzhakov <atanas.b@yahoo.com>
2024-11-08 13:01:57 +0200

    Add Home view

--- Exam preps/Renew Tech Cleanup/src/app.js
+++ Exam preps/Renew Tech Cleanup/src/app.js
@@ -4,8 +4,12 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
+import { renderHome } from './views/homeView.js';
+
 page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
+
+page('/', renderHome);
 
 page.start();
