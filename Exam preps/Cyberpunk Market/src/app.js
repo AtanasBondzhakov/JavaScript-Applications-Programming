a06620c056ee88618b7de65f511ef7ddd8eec73c
atanasbondzhakov <atanas.b@yahoo.com>
2024-11-13 15:00:53 +0200

    Add Login and Register logic

--- Exam preps/Cyberpunk Market/src/app.js
+++ Exam preps/Cyberpunk Market/src/app.js
@@ -5,11 +5,15 @@ import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMIddleware.js';
 
 import { renderHome } from './views/homeView.js';
+import { renderLogin } from './views/loginView.js';
+import { renderRegister } from './views/registerView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
 page('/', renderHome);
+page('/login', renderLogin);
+page('/register', renderRegister);
 
 page();
