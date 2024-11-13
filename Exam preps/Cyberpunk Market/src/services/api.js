1e2fe02106cdc0181c43e013b18ac6707c395634
atanasbondzhakov <atanas.b@yahoo.com>
2024-11-13 13:48:40 +0200

    Add services, middlewares, navigation and home logic

---
+++ Exam preps/Cyberpunk Market/src/services/api.js
@@ -0,0 +1,45 @@
+import { clearUserData, getUserData } from "../utils.js";
+
+const host = 'http://localhost:3030';
+
+const request = async (method, url, data) => {
+    const option = {
+        method,
+        headers: {}
+    }
+
+    const userData = getUserData();
+    if(userData) {
+        option.headers['X-Authorization'] = userData.accessToken;
+    }
+
+    if (data !== undefined) {
+        option.headers['Content-Type'] = 'application/json';
+        option.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(host + url, option);
+        if(!response.ok) {
+            const error = await response.json();
+            if (response.status === 403 && error.message === 'Invalid access token') {
+                clearUserData();
+            }
+            throw new Error(error.message);
+        }
+
+        if(response.status === 204) {
+            return response;
+        }
+
+        return response.json();
+    } catch (error) {
+        alert(error);
+        throw error;
+    }
+}
+
+export const get = (url) => request('GET', url);
+export const post = (url, data) => request('POST', url, data);
+export const put = (url, data) =>  request('PUT', url, data);
+export const del = (url) => request('DELETE', url);
---
+++ Exam preps/Cyberpunk Market/src/views/homeView.js
@@ -0,0 +1,10 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const homeTemplate = () => html`
+    <section id="hero">
+        <img src="./images/home.png" alt="home" />
+        <p>We know who you are, we will contact you</p>
+    </section>
+`;
+
+export const renderHome = (ctx) => ctx.render(homeTemplate());
